33993e7f34c572c7d8d883709a0e9c07a100a656
천상의나무 <ajb8533296@gmail.com>
2021-07-15 11:25:53 +0900

    패키지 준비

--- setup.py
+++ setup.py
@@ -1,36 +1,61 @@
-import os
+from setuptools import setup
+import platform
+import glob
 import sys
-from distutils.core import setup
+import os
 
-import py2exe
+if platform.system() == "Darwin":
+    APP = ['nabi.py']
+    DATA_FILES = [('assets/audio', glob.glob('assets/audio/*.*')),
+                ('assets/sprites', glob.glob('assets/sprites/*.*')),]
+    OPTIONS = {
+        'iconfile':'icon.icns'
+    }
 
-origIsSystemDLL = py2exe.build_exe.isSystemDLL
-def isSystemDLL(pathname):
-    dlls = ("libfreetype-6.dll", "libogg-0.dll", "sdl_ttf.dll")
-    if os.path.basename(pathname).lower() in dlls:
-        return 0
-    return origIsSystemDLL(pathname)
-py2exe.build_exe.isSystemDLL = isSystemDLL
+    setup(
+        name =    'The Adventure of Nabi',
+        version = '1.0',
+        author =  'An Jaebeom',
+        app=APP,
+        data_files=DATA_FILES,
+        setup_requires=['py2app'],
+        options={'py2app': OPTIONS},
+    )
 
-sys.argv.append('py2exe')
+elif platform.system() == "Windows":
+    import py2exe
 
-setup(
-    name =    'The Adventure of Nabi',
-    version = '1.0',
-    author =  'An Jaebeom',
-    options = {
-        'py2exe': {
-            'bundle_files': 1, # doesn't work on win64
-            'compressed': True,
-        }
-    },
+    origIsSystemDLL = py2exe.build_exe.isSystemDLL
+    def isSystemDLL(pathname):
+        dlls = ("libfreetype-6.dll", "libogg-0.dll", "sdl_ttf.dll")
+        if os.path.basename(pathname).lower() in dlls:
+            return 0
+        return origIsSystemDLL(pathname)
+    py2exe.build_exe.isSystemDLL = isSystemDLL
 
-    windows = [{
-        'script': "nabi.py",
-        'icon_resources': [
-            (1, 'icon.ico')
-        ]
-    }],
+    sys.argv.append('py2exe')
 
-    zipfile=None,
-)
+    setup(
+        name =    'The Adventure of Nabi',
+        version = '1.0',
+        author =  'An Jaebeom',
+        setup_requires=["py2app"],
+        options = {
+            'py2exe': {
+                'bundle_files': 1, # doesn't work on win64
+                'compressed': True,
+            }
+        },
+
+        windows = [{
+            'script': "nabi.py",
+            'icon_resources': [
+                (1, 'icon.ico')
+            ]
+        }],
+
+        zipfile=None,
+    )
+
+else:
+    print("X")
